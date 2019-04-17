5406bdd05e7a2e23d29851ec9fb0dcd8337a85d0
Deepen <dpen.connectify@gmail.com>
2019-04-17 22:04:35 +0545

    Changed CURL to WordPress native function

--- includes/views/html-payment-fields.php
+++ includes/views/html-payment-fields.php
@@ -1,53 +1,54 @@
 <?php
-if( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly.
 
 // TODO: Insert your custom payment fields here.
 ?>
 
 <script>
-jQuery(document).ready(function($){
-        jQuery('#payment_method_khalti').on('click',function(){
+    jQuery(document).ready(function ($) {
+        jQuery('#payment_method_khalti').on('click', function () {
             //jQuery('#place_order').hide();
         });
-        if(!document.getElementById('payment_method_khalti').checked)
-        { 
+        if (!document.getElementById('payment_method_khalti').checked) {
             jQuery('#place_order').show();
             alert('show');
         }
-})
+    })
 </script>
-    <script src="https://khalti.com/static/khalti-checkout.js"></script>
-    <!-- Place this where you need payment button -->
-    <button id="payment-button" style="background-color: #773292;color: #fff;border: none;padding: 5px 10px;border-radius: 2px;">Pay with Khalti</button>
-    <!-- Place this where you need payment button -->
-    <!-- Paste this code anywhere in you body tag -->
-    <script>
-        var config = {
-            // replace the publicKey with yours
-            "publicKey": "<?php echo $this->public_key;?>",
-            "productIdentity": "<?php echo $order_id;?>",
-            "productName": "product",
-            "productUrl": "url",
-            "eventHandler": {
-                onSuccess (payload) {
-                    // hit merchant api for initiating verfication
-                    //console.log(payload);
-                    var token = payload.token;
-                    var amount = payload.amount;
-                    window.location.href = '?token='+token+'&amount='+amount+'&order_id='+<?php echo $order_id;?>;
-                },
-                onError (error) {
-                    console.log(error);
-                },
-                onClose () {
-                    console.log('widget is closing');
-                }
+<script src="https://khalti.com/static/khalti-checkout.js"></script>
+<!-- Place this where you need payment button -->
+<button id="payment-button" style="background-color: #773292;color: #fff;border: none;padding: 5px 10px;border-radius: 2px;">Pay with Khalti</button>
+<!-- Place this where you need payment button -->
+<!-- Paste this code anywhere in you body tag -->
+<script>
+    var config = {
+        // replace the publicKey with yours
+        "publicKey": "<?php echo $this->public_key;?>",
+        "productIdentity": "<?php echo $order_id;?>",
+        "productName": "product",
+        "productUrl": "url",
+        "eventHandler": {
+            onSuccess(payload) {
+                // hit merchant api for initiating verfication
+                //console.log(payload);
+                var token = payload.token;
+                var amount = payload.amount;
+                window.location.href = '?token=' + token + '&amount=' + amount + '&order_id=' +<?php echo $order_id;?>;
+            },
+            onError(error) {
+                console.log(error);
+            },
+            onClose() {
+                console.log('widget is closing');
             }
-        };
+        }
+    };
 
-        var checkout = new KhaltiCheckout(config);
-        var btn = document.getElementById("payment-button");
-        //btn.onclick = function () {
-            checkout.show({amount: <?php echo $tot;?>});
-        //}
-    </script>
+    var checkout = new KhaltiCheckout(config);
+    var btn = document.getElementById("payment-button");
+    //btn.onclick = function () {
+    checkout.show({amount: <?php echo $tot;?>});
+    //}
+</script>
--- woo-includes/woo-functions.php
+++ woo-includes/woo-functions.php
@@ -2,14 +2,14 @@
 /**
  * Functions used by plugins
  */
-if( !class_exists( 'WC_Dependencies' ) ) {
-	require_once('class-wc-dependencies.php');
+if ( ! class_exists( 'WC_Dependencies' ) ) {
+	require_once( 'class-wc-dependencies.php' );
 }
 
 /**
  * WC Detection
  */
-if( !function_exists( 'is_woocommerce_active' ) ) {
+if ( ! function_exists( 'is_woocommerce_active' ) ) {
 	function is_woocommerce_active() {
 		return WC_Dependencies::woocommerce_active_check();
 	}
