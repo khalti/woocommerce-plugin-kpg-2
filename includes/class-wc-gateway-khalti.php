c0a719e6bdc6bc524c004a6b532250af1ce9a889
Bibek M Acharya <bibek.acharya@yipl.com.np>
2020-05-26 18:49:06 +0545

    Removed Force SSL warning on https

--- includes/class-wc-gateway-khalti.php
+++ includes/class-wc-gateway-khalti.php
@@ -256,7 +256,9 @@ class WC_Gateway_Khalti_Payment_Gateway extends WC_Payment_Gateway {
 			echo '<div class="error"><p>' . __( 'Khalti Error: Please enter your public and private keys', 'khalti' ) . '</p></div>';
 		} // Show message if enabled and FORCE SSL is disabled and WordPress HTTPS plugin is not detected.
 		else if ( 'no' == get_option( 'woocommerce_force_ssl_checkout' ) && ! class_exists( 'WordPressHTTPS' ) ) {
-			echo '<div class="error"><p>' . sprintf( __( 'Khalti is enabled, but the <a href="%s">force SSL option</a> is disabled; your checkout may not be secure! Please enable SSL and ensure your server has a valid SSL certificate - Gateway Name will only work in sandbox mode.', 'khalti' ), admin_url( 'admin.php?page=wc-settings&tab=checkout' ) ) . '</p></div>';
+			if(!is_ssl()){
+				echo '<div class="error"><p>' . sprintf( __( 'Khalti is enabled, but the <a href="%s">force SSL option</a> is disabled; your checkout may not be secure! Please enable SSL and ensure your server has a valid SSL certificate - Gateway Name will only work in sandbox mode.', 'khalti' ), admin_url( 'admin.php?page=wc-settings&tab=checkout' ) ) . '</p></div>';
+			}
 		}
 	}
 
